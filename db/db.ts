4046db0dc9b98f0d2094692104856454200e5688
Asad Ali Developer <uhope1645@gmail.com>
2025-01-18 09:56:48 +0500

    Improved Logs

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,13 @@
 import { yellow } from "colorette";
 import mongoose from "mongoose";
+import { logMessage } from "../services";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI!);
     // console.log("MongoDB connected successfully!");
-    console.log(yellow("[MongoDB] Connected successfully!"))
+    // console.log(yellow("[MongoDB] Connected successfully!"))
+    logMessage("[MongoDB] MongoDB connected successfully!");
   } catch (error) {
     console.log(error);
     process.exit(1); // Exit the process if connection fails
