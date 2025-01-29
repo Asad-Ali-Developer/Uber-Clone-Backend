67d6013885a0724457da51b009712c933c2cce00
Asad Ali Developer <uhope1645@gmail.com>
2025-01-29 08:06:59 +0500

    Removed Logs folder and logs file

--- db/db.ts
+++ db/db.ts
@@ -3,6 +3,7 @@ import { logMessage } from "../services";
 
 const connectDB = async () => {
   try {
+    // Changed the default setting to the MONGODB_URI environment variable
     await mongoose.connect(process.env.MONGODB_URI!);
     // console.log("MongoDB connected successfully!");
     // console.log(yellow("[MongoDB] Connected successfully!"))
